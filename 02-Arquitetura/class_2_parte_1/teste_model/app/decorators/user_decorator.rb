5868e76d7f042c3823329600c66dfc3c5c79a350
aureamar <aureamar.jose@gmail.com>
2025-05-13 02:12:50 +0000

    decorator I

---
+++ 02-Arquitetura/class_2_parte_1/teste_model/app/decorators/user_decorator.rb
@@ -0,0 +1,14 @@
+class UserDecorator
+  def initialize(user)
+    puts "---- UserDecorator initialized with user: #{user.inspect}"
+    @user = user
+  end
+
+  def name
+    @user.name.upcase
+  end
+
+  def email
+    @user.email.downcase
+  end
+end
